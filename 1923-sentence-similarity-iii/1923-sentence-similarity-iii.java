f489ec4ead0accd1893382ed9556591fb992d25a
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2024-10-06 19:02:23 +0530

    Time: 1 ms (96.43%), Space: 41.5 MB (83.33%) - LeetHub

---
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.java
@@ -0,0 +1,36 @@
+class Solution {
+    
+    public boolean areSentencesSimilar(String s1, String s2) {
+        if(s1.length() < s2.length()){
+            String temp = s1;
+            s1 = s2;
+            s2 = temp;
+        }
+
+String ar1[]=s1.split(" ");
+String ar2[]=s2.split(" ");
+
+        int i=0,j=ar1.length-1;
+        int k=0,l=ar2.length-1;
+
+        while(k<=l && i<=j){
+            if(ar2[k].equals(ar1[i])){
+                k++;
+                i++;
+            }
+            else{
+                break;
+            }
+        }
+        while(l>=k){
+            if(ar2[l].equals(ar1[j])){
+                j--;
+                l--;
+            }
+            else{
+                break;
+            }
+        }
+        return l<k;
+    }
+}

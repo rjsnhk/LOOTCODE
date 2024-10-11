4f60b321369781881db0e5b59d89930e0d6c15ad
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2024-10-11 23:59:27 +0530

    Time: 46 ms (77.78%), Space: 48.5 MB (78.95%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.java
@@ -1,30 +1,35 @@
 class Solution {
+/*
+1.Store the arrivalTime of targetFriend, since all the the arrival times are distinct this can be used to identify the targetFriend in sorted array.
+2.Store all the available seats(one for each friend) in minHeap as available seats.
+3.Sort the times array based on arrival time.
+4.Maintain another minHeap to store leavingTime and occupiedSeat.
+5.loop through sorted array
+    1) remove all the friends from the heap whose leavingTime is less than or equal to current arraival time and add the occupied seats back to seats heap.
+    2) if the current start is equal to targetStart return the min available seat.
+    3) else add the current leaving time and min available seat to heap.
+*/    
     public int smallestChair(int[][] times, int targetFriend) {
-        int n=times.length;
-int seat[]=new int[n];
-Arrays.fill(seat,-1);
-
-        int targetArrival=times[targetFriend][0];
-        // Sort the 2D array based on the first column
-Arrays.sort(times, Comparator.comparingInt(a -> a[0]));
-
-
-
-for(int time[]:times){
-    int arrival=time[0];
-    int depart=time[1];
-    for(int i=0;i<n;i++){
-if(seat[i]<=arrival){
-    seat[i]=depart;
-
-if(arrival==targetArrival){
-    return i;
-}
-break;
-    }
-    }
-}
-return -1;
-
+        int targetStart = times[targetFriend][0];
+        Arrays.sort(times, (a, b) -> a[0] - b[0]); // sort by start time
+        
+        PriorityQueue<Integer> seats = new PriorityQueue<>(); // available seat
+        for (int i = 0; i < times.length; ++ i) {
+            seats.offer(i);
+        }
+        
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]); // leavingTime(sorted), occupiedSeat
+        
+        for (int i = 0; i < times.length; ++ i) {
+            while (!minHeap.isEmpty() && minHeap.peek()[0] <= times[i][0]) { // poll friends whose leavingTime is less than or equal to current arraival time 
+                seats.offer(minHeap.poll()[1]); // add the occupied seats back to available heap
+            }
+            
+            if (times[i][0] == targetStart) return seats.peek();
+            
+            minHeap.offer(new int[]{times[i][1], seats.poll()});
+        }
+        
+        return seats.peek();
     }
 }

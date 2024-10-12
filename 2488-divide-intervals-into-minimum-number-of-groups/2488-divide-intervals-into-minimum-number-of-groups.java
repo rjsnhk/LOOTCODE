9892504c832dabf467e7593d6fbe460b4d6614c4
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2024-10-12 19:08:35 +0530

    Time: 94 ms (60.87%), Space: 75.6 MB (75.27%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
@@ -4,15 +4,13 @@ class Solution {
         Arrays.sort(intervals,(a,b)->(a[0]==b[0])?a[1]-b[1]:a[0]-b[0]);
 
         for(int inter[]:intervals){
-            int st=inter[0];
-            int end=inter[1];
 
             
-                if(!ar.isEmpty()&&ar.peek()<st){
+                if(ar.size()>0 &&ar.peek()<inter[0]){
                     ar.remove();
-                    ar.add(end);
+                    ar.add(inter[1]);
                 }else
-                    ar.add(end);
+                    ar.add(inter[1]);
                 
         }
         return ar.size();

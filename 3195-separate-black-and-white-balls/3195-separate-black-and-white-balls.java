5a2011966479afbc9d5da8a255c1f877d079c17f
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2024-10-15 22:00:47 +0530

    Time: 7 ms (100%), Space: 45.3 MB (54.12%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -1,16 +1,21 @@
 class Solution {
+
     public long minimumSteps(String s) {
-          long ans=0;
-          char arr[]=s.toCharArray();
-          int countone=0;
-          for(int i=0;i<arr.length;i++){
-            if(arr[i]=='1'){
-                countone++;
+        int whitePosition = 0;
+        long totalSwaps = 0;
+
+        // Iterate through each ball (character) in the string
+        for (int currentPos = 0; currentPos < s.length(); currentPos++) {
+            if (s.charAt(currentPos) == '0') {
+                // Calculate the number of swaps needed
+                // to move it to leftmost available position
+                totalSwaps += currentPos - whitePosition;
+
+                // Move the next available position for a white ball one step to the right
+                whitePosition++;
             }
-            if(arr[i]=='0'){
-                ans+=countone;
-            }
-          }
-return ans;
+        }
+
+        return totalSwaps;
     }
 }

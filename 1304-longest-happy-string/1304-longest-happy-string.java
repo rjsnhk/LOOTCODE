9231a2b551b6e59fdb8d4614484237db5414374e
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2024-10-16 22:40:04 +0530

    Time: 0 ms (100%), Space: 41.1 MB (26.05%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.java
+++ 1304-longest-happy-string/1304-longest-happy-string.java
@@ -1,63 +1,31 @@
 class Solution {
-
-    public char max(int a, int b, int c) {
-        int max = Math.max(a, Math.max(b, c));
-        if (max == a) {
-            return 'a';
-        } else if (max == b) {
-            return 'b';
-        } else {
-            return 'c';
-        }
-    }
-
     public String longestDiverseString(int a, int b, int c) {
-        StringBuilder ans = new StringBuilder();
-        int countA = 0, countB = 0, countC = 0;
-
-        while (a > 0 || b > 0 || c > 0) {
-            char nextChar = max(a, b, c);
-
-            if ((nextChar == 'a' && countA == 2) || (nextChar == 'b' && countB == 2) || (nextChar == 'c' && countC == 2)) {
-                // If adding the max character results in three consecutive characters, choose another character
-                if (nextChar == 'a' && b > 0) {
-                    nextChar = 'b';
-                } else if (nextChar == 'a' && c > 0) {
-                    nextChar = 'c';
-                } else if (nextChar == 'b' && a > 0) {
-                    nextChar = 'a';
-                } else if (nextChar == 'b' && c > 0) {
-                    nextChar = 'c';
-                } else if (nextChar == 'c' && a > 0) {
-                    nextChar = 'a';
-                } else if (nextChar == 'c' && b > 0) {
-                    nextChar = 'b';
-                } else {
-                    // No valid character to choose from
-                    break;
-                }
-            }
-
-            // Add the chosen character to the answer and update counts
-            ans.append(nextChar);
-            if (nextChar == 'a') {
+        StringBuilder sb = new StringBuilder();
+        
+        int totalLength = a + b + c;
+        int continuousA = 0, continuousB = 0, continuousC = 0;
+        
+        for(int i = 0; i < totalLength; i++) {
+            if ((a >= b && a >= c && continuousA != 2) || (continuousB == 2 && a > 0) || (continuousC == 2 && a > 0))  {
+                sb.append("a");
                 a--;
-                countA++;
-                countB = 0;
-                countC = 0;
-            } else if (nextChar == 'b') {
+                continuousA++;
+                continuousB = 0;
+                continuousC = 0;  
+            } else if ((b >= a && b >= c && continuousB != 2) || (continuousA == 2 && b > 0) || (continuousC == 2 && b > 0)) {
+                sb.append("b");
                 b--;
-                countB++;
-                countA = 0;
-                countC = 0;
-            } else {
+                continuousB++;
+                continuousA = 0;
+                continuousC = 0;
+            } else if ((c >= a && c >= b && continuousC != 2) || (continuousB == 2 && c > 0) || (continuousA == 2 && c > 0)) {
+                sb.append("c");
                 c--;
-                countC++;
-                countA = 0;
-                countB = 0;
+                continuousC++;
+                continuousA = 0;
+                continuousB = 0;  
             }
         }
-
-        return ans.toString();
+        return sb.toString();
     }
 }

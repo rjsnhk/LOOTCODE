2d12f2b70260e316aafcae9fb3a937bd17abf833
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2024-11-04 23:07:52 +0530

    Time: 1965 ms (5.08%), Space: 51.5 MB (11.67%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.java
+++ 3451-string-compression-iii/3451-string-compression-iii.java
@@ -1,40 +1,22 @@
 class Solution {
     public String compressedString(String w) {
-        if(w.length()==1){
-            return 1+w;
-        }
         String comp="";
-        int count=1;
-        char a=w.charAt(0);
-        for(int i=1;i<w.length();i++){
-            
-            if(count==9){
-                comp+=count;
-                comp+=a;
-                a=w.charAt(i);
-                count=1;
-            }
-           
-            else if(a!=w.charAt(i)){
-                comp+=count;
-                comp+=a;
-                a=w.charAt(i);
-                count=1;
-            }
-            else if(a==w.charAt(i)){
-                count++;
-            }
-            if(i==w.length()-1){
-                comp+=count;
-                comp+=a;
-            }
-            
-            
-            
-            
-            
+        int n=w.length();
+int i=0;
+while(i<n){
+
+        int count=0;
+        char ch=w.charAt(i);
+        while(i<n && count<9 && ch==w.charAt(i)){
+            count++;
+            i++;
         }
+        comp+=count;
+        comp+=ch;
         
+}
+
+
         return comp;
     }
 }

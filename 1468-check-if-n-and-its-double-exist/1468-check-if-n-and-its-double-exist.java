9109c3dc8e816d6239fcbf9f19552520ccaad47c
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2024-12-01 12:31:28 +0530

    Time: 2 ms (93.52%), Space: 43.7 MB (10.32%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -1,16 +1,11 @@
+import java.util.HashSet;
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i:arr){
-           if(!hm.containsKey(i)) hm.put(i,1);
-           else{
-            hm.put(i,hm.get(i)+1);
-           }
-        }
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==0&&hm.get(0)>=2){return true;}
-            if(arr[i]!=0&&hm.containsKey(arr[i]*2)) return true;
-            
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            if (set.contains(i * 2)) return true;
+            if (i % 2 == 0 && set.contains(i / 2)) return true;
+            set.add(i);
         }
         return false;
     }

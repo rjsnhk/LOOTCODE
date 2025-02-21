3a44eddf6a35e63fd955a3583c8db49470dcf59b
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2025-02-21 23:56:32 +0530

    Time: 20 ms (98.92%), Space: 48 MB (16.17%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
@@ -14,53 +14,21 @@
  * }
  */
 class FindElements {
-    TreeNode root;
-    public FindElements(TreeNode root) {
-        this.root=root;
-        Queue<TreeNode> q=new LinkedList<>();
-        root.val=0;
-        q.add(root);
-        q.add(null);
-        while(!q.isEmpty()){
-            TreeNode curr=q.remove();
-            if(curr==null){
-                if(q.isEmpty()) break;
-                else q.add(null);
-            }
-                else{
-                    if(curr.left!=null) {q.add(curr.left);
-                    curr.left.val=2*curr.val+1;
-                    };
-                    if(curr.right!=null) {q.add(curr.right);
-                    curr.right.val=2*curr.val+2;
-                    }
-                }
-            }
+    HashSet<Integer> hs=new HashSet<>();
+    public void buildTree(TreeNode root,int val){
+        if(root==null) return;
+        root.val=val;
+        hs.add(val);
+        buildTree(root.left,2*val+1);
+        buildTree(root.right,2*val+2);
 
-        }
-    
+    }
+    public FindElements(TreeNode root) {
+        buildTree(root,0);
+    }
     
     public boolean find(int target) {
-        Queue<TreeNode> q=new LinkedList<>();
-        root.val=0;
-        q.add(root);
-        q.add(null);
-        while(!q.isEmpty()){
-            TreeNode curr=q.remove();
-            if(curr==null){
-                if(q.isEmpty()) break;
-                else q.add(null);
-            }
-                else{
-                    if(curr.val==target) return true;
-                    if(curr.left!=null) {q.add(curr.left);
-                    };
-                    if(curr.right!=null) {q.add(curr.right);
-                    }
-                }
-            }
-            return false;
-        
+        return hs.contains(target);
     }
 }
 

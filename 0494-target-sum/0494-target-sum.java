da0d34892b622fbc5d0be059ddc14491343b19c1
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2025-02-27 15:30:29 +0530

    Time: 5 ms (88.28%), Space: 43.8 MB (45.52%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countTarget(int ar[],int t,int n){
+        int dp[][]=new int[n+1][t+1];
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<t+1;j++){
+                if(ar[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-ar[i-1]]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+
+        }
+        return dp[n][t];
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+    int sum=0;
+    for(int i:nums) sum+=i;
+    if((sum+target)%2!=0 || sum < Math.abs(target)) return 0;
+    int t=(sum+target)/2;
+    return countTarget(nums,t,nums.length);
+    }
+}

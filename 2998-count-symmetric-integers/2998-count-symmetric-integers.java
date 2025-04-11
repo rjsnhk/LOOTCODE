3a15be0d95f494481fccf0589811381ca2f58eca
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2025-04-11 09:38:02 +0530

    Time: 26 ms (74.03%), Space: 44.7 MB (52.09%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.java
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.java
@@ -1,29 +1,18 @@
 class Solution {
-    
-    int sum(int num,int l){
-        int s=0;
-        for(int i=0;i<l;i++){
-            s+=num%10;
-            num/=10;
-        }
-        return s;
-    }
-    boolean sym(int n){
-        String num=Integer.toString(n);
-        int l=num.length();
-        if(l%2!=0) return false;
-        int s=sum(n,l);
-        if(s%2!=0) return false;
-        int half=sum(n,l/2);
-        if(s/2==half) return true;
-        else return false;
-
-
-    }
     public int countSymmetricIntegers(int low, int high) {
         int c=0;
         for(int i=low;i<=high;i++){
-            if(sym(i)) c++;
+            String num=Integer.toString(i);
+            int len=num.length();
+            if(len%2!=0) continue;
+            int half=len/2;
+            int sum1=0;
+            int sum2=0;
+            for(int j=0;j<half;j++){
+                sum1+=num.charAt(j);
+                sum2+=num.charAt(half+j);
+            }
+            if(sum1==sum2) c++;
         }
         return c;
     }
